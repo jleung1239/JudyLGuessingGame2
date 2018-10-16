e80f959f60fde34d3bf5fe3db9f40daf32f9e53c
jleung1239 <jleung1239@bths.edu>
2018-10-15 21:52:16 -0400

    a

--- src/JudyL/Main.java
+++ src/JudyL/Main.java
@@ -6,10 +6,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // JudyLLib.GuessGame1(0);
+        JudyLLib.GuessGame1(0);
 
        JudyLLib.GuessGame2(0);
 
-	// write your code here
+	// GuessGame1 is the user guessing the computer's number
+        // GuessGame2 is the computer guessing the user's number
+        // to run only one of the games, put a "//" in front of the one you want to disable
     }
 }
